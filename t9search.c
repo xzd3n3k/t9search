7ed55fc02835992eb8b9e019380e648c4aa1b192
Zdeněk Němec <xzdeneknemec@outlook.cz>
2022-10-12 23:59:27 +0200

    init

---
+++ t9search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc > 2) {
+        printf("Error: too many arguments, expected 1, taken %d\n", argc-1);
+        return 1;
+    }
+
+    else if (argc == 2) {
+        char *number = argv[1];
+        printf("%s\n", number);
+    }
+
+    else {
+
+        while (1) {
+            int character = fgetc(stdin);
+
+            if (character == EOF) break;
+            putchar(character);
+            //printf("%c", character);
+        }
+        
+    }
+    
+    
+    return 0;
+}
