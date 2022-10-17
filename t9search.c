cf76d502d8fcc6661cf4f55b1b0e2c2d9a83d127
Zdeněk Němec <xzdeneknemec@outlook.cz>
2022-10-17 20:15:05 +0200

    again fixed filter function, added switch default, some minor changes

--- t9search.c
+++ t9search.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #define MAX_RECORD_LEN 100
-//TODO tolowercase names
+
+//TODO tolowercase names, comment, edit filter function and in main edit mess w numbers variables + when found from text dont print not found from numbers + when found from text print also their number
 /*int count_lines() { // function to count and return how many records do i have from stdin
     char line[MAX_RECORD_LEN];
     int counter = 0;
@@ -62,7 +64,7 @@ int len(char *str) { // function that returns len of given string
 int includes(char *big, char small) { // function to check if string contains exact letter returns 1 when it does and 0 when it doesnt
     int len_big = len(big);
     for (int j = 0; j < len_big; j++) {
-        if (big[j] == small) return 1;
+        if (big[j] == tolower(small)) return 1;
     }
     
     return 0;
@@ -75,6 +77,7 @@ int filter(char *text, char *number) {
     int def = 0; // default - when should it start to search again when there are multiple occurences of same letter
     int iter = 0; // given text iterator
     int match = 0; // match counter
+    int auxiliary_filter_found = 0;
 
     for (int i = 0; i < number_len; i++) {
 
@@ -106,6 +109,8 @@ int filter(char *text, char *number) {
             case '9':
                 letters = "wxyz";
                 break;
+            default:
+                return 0;
         }
         //printf("JSEM ZA SWITCHEM\n");
         for (int x = 0; x < len(text)-def; x++) { // iterating text
@@ -113,7 +118,7 @@ int filter(char *text, char *number) {
                 //printf("FOUND text iter true: %c, letters: %s, iter: %d, i: %d, text: %s\n", text[iter], letters, iter, i, text);
                 iter++; // increment iterator
                 match++; // increment match
-                if (match == number_len) {printf("SHODA: %s\n", text);} // checking if its OK
+                if (match == number_len) {printf("SHODA: %s\n", text); auxiliary_filter_found += 1;} // checking if its OK
                 break; // break cycle to continue matching
                 }
             //printf("NOT FOUND text iter true: %c, letters: %s, iter: %d, i: %d, number[i]: %c\n", text[iter], letters, iter, i, number[i]);
@@ -127,7 +132,7 @@ int filter(char *text, char *number) {
         }
         
     }
-    return 0;
+    return auxiliary_filter_found;
 }
 
 int main(int argc, char *argv[]) {
@@ -145,6 +150,8 @@ int main(int argc, char *argv[]) {
         char line[MAX_RECORD_LEN]; // declared variable for name-record
         char number_line[MAX_RECORD_LEN]; // declared variable for number-record
         int auxiliary_found = 0; // auxiliary variable to determinate if at least one contact was found or not
+        int filter_found;
+        int filter_found_result = 0;
 
         while (fgets(line, MAX_RECORD_LEN+1, stdin)) // while records are available
         {
@@ -152,17 +159,36 @@ int main(int argc, char *argv[]) {
             //char *number_line_wo_nwl = rem_newline(number_line); // variable to store number-record without newline
             //printf("numberline wo nwl: %s\n", number_line_wo_nwl);
             //filter(line_wo_nwl, number_line_wo_nwl);
-            filter(line_wo_nwl, rem_newline(rem_spaces(number)));
-            fgets(number_line, MAX_RECORD_LEN+1, stdin); // getting number-record and storing into number_line
+            filter_found = filter(line_wo_nwl, rem_newline(rem_spaces(number)));
+
+            if (filter_found > 0) {
+                filter_found_result++;
+                fgets(number_line, MAX_RECORD_LEN+1, stdin); // getting number-record and storing into number_line
+                char *number_line_wo_nwl = rem_newline(number_line); // variable to store number-record without newline
+                printf("NALEZENO pres text: %s, %s\n", line_wo_nwl, number_line_wo_nwl);
+
+            }
+
+            else if (filter_found == 0) {
+
+                fgets(number_line, MAX_RECORD_LEN+1, stdin); // getting number-record and storing into number_line
+
+                char *number_line_wo_nwl = rem_newline(number_line); // variable to store number-record without newline
+                char *edited_number = rem_spaces(number_line); // variable to store number without spaces
+                char *found = strstr(edited_number, number); // variable to store if substring was found or not
+
+            if (found != NULL) {auxiliary_found++; printf("NALEZENO: %s, %s\n", line_wo_nwl, number_line_wo_nwl);} // if substring was found increment auxiliary variable and print contact
+            }
+            /*fgets(number_line, MAX_RECORD_LEN+1, stdin); // getting number-record and storing into number_line
 
             char *number_line_wo_nwl = rem_newline(number_line); // variable to store number-record without newline
             char *edited_number = rem_spaces(number_line); // variable to store number without spaces
             char *found = strstr(edited_number, number); // variable to store if substring was found or not
 
-            if (found != NULL) {auxiliary_found++; printf("NALEZENO: %s, %s\n", line_wo_nwl, number_line_wo_nwl);} // if substring was found increment auxiliary variable and print contact
+            if (found != NULL) {auxiliary_found++; printf("NALEZENO: %s, %s\n", line_wo_nwl, number_line_wo_nwl);} // if substring was found increment auxiliary variable and print contact*/
         }
 
-        if (auxiliary_found == 0) {printf("Not found");} // if none contact was found print message
+        if (auxiliary_found == 0 && filter_found_result == 0) {printf("Not found");} // if none contact was found print message
 
         return 0;
         
